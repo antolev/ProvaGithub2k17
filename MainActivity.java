8eeb7bb1df5b2b31afd96d49db62325978024d85
antolev <antonioleva89@gmail.com>
2017-01-02 10:09:26 +0100

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package alo.com.provagithub2k17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //Primo Leva
+    }
+}
